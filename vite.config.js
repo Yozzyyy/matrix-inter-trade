47c011bb116b7aa7327da4e17159da5122229b49
Bryan <Yapbryan602@gmail.com>
2026-08-17 15:23:21 +0800

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/matrix-inter-trade', 
+  base: '/matrix-inter-trade/',
   plugins: [
     react(),
     tailwindcss(),
